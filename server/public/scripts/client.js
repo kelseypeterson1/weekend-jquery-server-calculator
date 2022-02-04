b61da4b74f5716c5a63ba14d395fe20ed1ad408f
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-04 16:53:38 -0600

    number inputs are successfully sent between the client and the server through an ajax POST function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,22 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('test')
+    $('#equals-sign').on('click', sendInputs)
 }
+
+function sendInputs() {
+    $.ajax({
+        method: 'POST',
+        url: '/numbers',
+        data: {
+            firstNumber: $('#first-number-input').val(),
+            secondNumber: $('#second-number-input').val(),
+        }
+    }).then(function(response) {
+        console.log('Sent numbers to the server');
+
+    }).catch(function(response) {
+        console.log('Wasn\'t able to send numbers to the server', response)
+    })
+}
+
--- server/server.js
+++ server/server.js
@@ -8,6 +8,18 @@ const port = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
+// this POST will receive numbers from the client
+app.post('/numbers', function(req, res) {
+    let firstNumber = req.body.firstNumber;
+    let secondNumber = req.body.secondNumber;
+    console.log("First number is", firstNumber)
+    console.log("Second number is", secondNumber)
+    // send back a status code of 201
+    res.sendStatus(201);
+})
+
 // express is listening and ready to host the server
 app.listen(port, function() {
     console.log('listening on port', port);
